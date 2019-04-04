7bca7b598f40772444fc116c34ed26552b860fcf
Beatriz Leon <beatriz@shadowrobot.com>
2019-04-04 11:33:48 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -207,7 +207,7 @@ intersphinx_mapping = {'https://docs.python.org/': None}
 # app setup hook
 def setup(app):
     app.add_config_value('recommonmark_config', {
-        'url_resolver': lambda url: github_doc_root + url,
+        #'url_resolver': lambda url: github_doc_root + url,
         'auto_toc_tree_section': 'Contents',
         'enable_eval_rst': True,
         'enable_auto_doc_ref': True,
