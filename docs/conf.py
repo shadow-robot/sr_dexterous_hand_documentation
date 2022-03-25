49b229598e958c066d63966258b0a5bfdb2525ec
maxzieba <max@shadowrobot.com>
2022-03-25 14:39:09 +0000

    test

--- docs/conf.py
+++ docs/conf.py
@@ -25,6 +25,7 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath('../'))
+autodoc_mock_imports = ['test_package']
 
 
 # -- Project information -----------------------------------------------------
