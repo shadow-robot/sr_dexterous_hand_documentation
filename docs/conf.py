6f50b4b85f7228eb3fcb2c7f87d668a02f7b59fe
Beatriz Leon <beatriz@shadowrobot.com>
2022-06-03 06:19:20 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -189,7 +189,7 @@ latex_theme_path = ['_static']
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
     (master_doc, 'DexterousHand.tex', u'Dexterous Hand Documentation',
-     u'Shadow Robot Company', 'cls/shadow-manual'),
+     u'Shadow Robot Company', 'shadow-manual'),
 ]
 
 
