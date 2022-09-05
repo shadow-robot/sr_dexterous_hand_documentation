7bd4d6ed39b6bdb2424ce3e3aaf979f6657f0625
Beatriz Leon <beatriz@shadowrobot.com>
2022-09-05 12:55:30 -0500

    Tesing version

--- docs/conf.py
+++ docs/conf.py
@@ -39,7 +39,7 @@ github_doc_root = 'https://github.com/shadow-robot/sr_documentation/tree/master/
 
 # The short X.Y version
 current_branch = Repository('.').head.shorthand
-current_branch = str(list(Repository('.').branches.local))
+current_branch = str(list(Repository('.').branches))
 
 version = current_branch
 # The full version, including alpha/beta/rc tags
