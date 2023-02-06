19ced32cca90ea6431ebbaef0ae41cd8954606a4
Beatriz Leon <beatriz@shadowrobot.com>
2023-02-06 10:18:02 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -56,7 +56,6 @@ extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
-    'notfound.extension',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
